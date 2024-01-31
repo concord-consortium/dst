ab8591e8bca9e54a1c72b4d71de7bc3fb7a4fd53
Doug Martin <dmartin@concord.org>
2024-01-31 15:34:05 -0500

    feat: Added react-leaflet map [PT-#186955135]
    This is set with a placeholder pin to show it works.

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,7 +1,37 @@
-import Wireframe from "./wireframe"
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet'
+import { LatLngExpression } from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+const ccEast: LatLngExpression = [42.456614,-71.3607501]
+
+function MapClickLayer() {
+  useMapEvents({
+    click: (e) => {
+      console.log("CLICKED", e.latlng)
+      // TODO: find closest position in dataset and set selected position
+      // in global state
+    },
+  })
+  return null
+}
 
 function Map() {
-  return <Wireframe>Map</Wireframe>
+  return (
+    <MapContainer center={ccEast} zoom={7} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={ccEast}>
+        <Popup>
+          Concord Consortium East. <br /> 25 Love Lane, Concord, MA.
+        </Popup>
+      </Marker>
+      <MapClickLayer />
+    </MapContainer>
+  )
 }
 
 export default Map
+
