2562a7ae6cd478d3a4f7e6e2628a0cc0747a308a
lublagg <lvblagg@gmail.com>
2024-02-02 10:57:38 -0500

    Add 'base' to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // empty string so that relative paths are used when viewing branches
+  base: ''
 })
